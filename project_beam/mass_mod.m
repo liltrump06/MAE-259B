af8667d419f78de112fd79c3e139f16867529e6e
liltrump06 <102622349+liltrump06@users.noreply.github.com>
2022-05-19 15:37:21 -0700

    Delete project_beam directory

--- project_beam/mass_mod.m
+++
@@ -1,9 +0,0 @@
-function mass_mod_and_z(q_new,q_old,M,conmap,velop)
-global N stack
-for i =1:N
-    k = (q_new(2*i-1)-q_old(2*i-1))/(q_new(2*i)-q_old(2*i));
-    b = q_new(2*i-1);
-    vp = velop(i,:);
-    vp_normal = dot(vp,nstack(ypos))*nstack(ypos);
-
-
--- project_beam/nstack.m
+++
@@ -1,4 +0,0 @@
-function n = nstack(ypos,stack)
-ny = -(2*stack(1)*ypos+stack(2));
-nx = 1;
-n = [nx;ny]/norm([nx;ny]);
